5af898cc2abf18fab4d85077ae8e9922c492f8a2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-31 07:37:03 +0900

    ✨ 履歴スタックを置き換え

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const SettingsPage = () => {
-  return <div>SettingsPage</div>
+  const pathname = usePathname()
+
+  const switchLocale = (locale: string) => {
+    const newPath = `${pathname}/${locale}`
+    // 最後に置き換えたパスに繊維（enの場合settings/enに遷移する（前の履歴（/settingsを置き換える）が追加されず置き換えるため））
+    window.history.replaceState(null, '', newPath)
+  }
+
+  return (
+    <>
+      <button type="button" onClick={() => switchLocale('en')}>
+        English
+      </button>
+      <button type="button" onClick={() => switchLocale('fr')}>
+        French
+      </button>
+    </>
+  )
 }
 
 export default SettingsPage
