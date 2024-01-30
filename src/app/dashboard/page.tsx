25fdc7fb65e389c162496357b1fc6bff50f3c633
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-31 07:25:55 +0900

    ✨ ブラウザの履歴スタックにパスを追加

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>
+  const searchParams = useSearchParams()
+
+  const updateSorting = (sortOrder: string) => {
+    const params = new URLSearchParams(searchParams.toString())
+
+    params.set('sort', sortOrder)
+    window.history.pushState(null, '', `/dashboard?${params.toString()}`)
+  }
+  return (
+    <>
+      <button type="button" onClick={() => updateSorting('asc')}>
+        Sort Ascending
+      </button>
+      <button type="button" onClick={() => updateSorting('desc')}>
+        Sort Descending
+      </button>
+    </>
+  )
 }
 
 export default DashboardPage
